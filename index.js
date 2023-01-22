483cd4056c35841e127c2512c33b85febdc04036
Paul Ashby <paul@primitive.co>
2023-01-22 15:45:28 +0000

    Handle spaces in title, add table of contents

--- index.js
+++ index.js
@@ -1,59 +1,7 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
+const questions = require("./utils/questions");
 const generateMarkdown = require("./utils/generateMarkdown");
-
-// array of questions for user
-const questions = [
-    {
-        type: 'input',
-        name: 'username',
-        message: "What is your git user name?",
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: "What is your email address?",
-    },
-    {
-        type: 'input',
-        name: 'title',
-        message: "What is the title of your project?",
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: "Please enter a description of your project",
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: "What are the installation instructions?",
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: "How will your project be used?",
-    },
-    {
-        type: 'input',
-        name: 'contributing',
-        message: "How can people contribute",
-    },
-    {
-        type: 'input',
-        name: 'tests',
-        message: "How should tests be handled?",
-        default: "Standard"
-    },
-    {
-        type: 'list',
-        name: 'license',
-        message: "What license will your project use?",
-        choices: ["MIT", "Apache", "GPL", "BSD"],
-        default: 0
-    }
-
-];
 
 // function to write README file
 function writeToFile(fileName, data) {
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,38 +1,57 @@
-
 const path = require('path');
+const queryString = require('querystring');
 
 // function to generate markdown for README
 function generateMarkdown(data) {
 
+  // Destructure data parameter
   const { username, email, title, description, installation, usage, contributing, tests, license } = data;
-  
+  // Handle spaces in title
+  const emailSubject = queryString.escape(title);
+  // Configure license options
   const licensePaths = {
     MIT: {
-      license: "https://opensource.org/licenses/MIT", 
+      license: "https://opensource.org/licenses/MIT",
       badge: "License-MIT-yellow.svg"
     },
     Apache: {
-      license: "https://opensource.org/licenses/Apache-2.0", 
+      license: "https://opensource.org/licenses/Apache-2.0",
       badge: "License-Apache%202.0-blue.svg"
     },
     GPL: {
-      license: "https://www.gnu.org/licenses/gpl-3.0", 
+      license: "https://www.gnu.org/licenses/gpl-3.0",
       badge: "License-GPL%20v3-blue.svg"
     },
     BSD: {
-      license: "https://opensource.org/licenses/BSD-3-Clause", 
+      license: "https://opensource.org/licenses/BSD-3-Clause",
       badge: "License-BSD%203--Clause-blue.svg"
     }
   }
+  const badge = `https://img.shields.io/badge/${licensePaths[license].badge}`;
 
-  const badge = path.format({
-    dir: 'https://img.shields.io/badge',
-    base: licensePaths[license].badge
-  });
+  let contents = "";
 
-  const licenseUrl = licensePaths[license].license;  
-  
-  return `# ${title}
+  for (const [key, value] of Object.entries(data)) {
+    // Don't want sections for sername or email
+    if (key !== "username" && key !== "email" && key !== "title") {
+      // Normalise key case
+      const lc = key.toLowerCase();
+      // Use sentence case
+      const capitalised = `${key.charAt(0).toUpperCase()}${key.slice(1)}`;
+      // Add to contents list
+      contents += `[${capitalised}](#${key})<br />`;
+    }
+}
+
+// Assemble markup
+return `# ${title}
+
+## Table of Contents
+
+  ${contents}
+
+  ## Description
+
   ${description}
   
   ## Installation
@@ -53,7 +72,7 @@ function generateMarkdown(data) {
   
   ## Questions
   
-  If you have any queries, don't hesitate to [contact me](mailto:${email}?subject=${title}).
+Feel free to [email me](mailto:${email}?subject=${emailSubject}) if you have any queries. If you'd be interested in finding out about other projects I'm working on, please check out my [GitHub profile](https://github.com/${username}).
   
   ## License
   
